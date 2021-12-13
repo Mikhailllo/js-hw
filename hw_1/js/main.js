0ef5d467b85bbb1abb92d1f409c36694758e22b2
admin <admin@MacBook-Pro-admin.local>
2021-12-14 01:32:31 +0200

    added a value,brought out the max and min numbers

--- hw_1/js/main.js
+++ hw_1/js/main.js
@@ -1,1 +1,31 @@
-console.log("hello world")
+const tvPrice = 15.678;
+const phonePrice = 123.965;
+const headphonesPrice = 90.2345;
+const maxPrice = Math.max(tvPrice, phonePrice, headphonesPrice);
+console.log(maxPrice);
+const minPrice = Math.min(tvPrice, phonePrice, headphonesPrice);
+console.log(minPrice);
+const sumPrice = tvPrice + phonePrice + headphonesPrice;
+console.log(sumPrice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
