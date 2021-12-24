6226edd72f8639550a9ff6409ab345b5f7d1fa52
admin <admin@MacBook-Pro-admin.local>
2021-12-24 18:11:15 +0200

    completed hw task

--- hw_2/js2/main.js
+++ hw_2/js2/main.js
@@ -1,21 +1,45 @@
-
-const n = prompt('add Start Volue');
-const m = prompt("add end Volume");
-const volue = confirm("пропускати парні числа");
-
-const notEvenNumber = true;
-parseInt(n);
-console.log(Number.isInteger(n));
-parseInt(m);
-console.log(Number.isInteger(m));
-let sum = 0;
-for (let i = n; i <= m; i++) {
-    if (!notEvenNumber || i % 2 === 1) {
-        sum += i;
+const n = 1;
+const m = 10;
+const value = confirm("пропускати парні числа");
+if (value) {
+    const notEvenNumber = true;
+    parseInt(n);
+    console.log(Number.isInteger(n));
+    parseInt(m);
+    console.log(Number.isInteger(m));
+    let sum = 0;
+    for (let i = n; i <= m; i++) {
+        if (!notEvenNumber || i % 2 === 1) {
+            sum = sum + i;
+        }
     }
     console.log(sum);
 
 }
+else {
+    const notEvenNumber = false;
+    parseInt(n);
+    console.log(Number.isInteger(n));
+    parseInt(m);
+    console.log(Number.isInteger(m));
+    let sum = 0;
+    for (let i = n; i <= m; i++) {
+        if (!notEvenNumber || i % 2 === 1) {
+            sum = sum + i;
+        }
+    }
+    console.log(sum);
+        
+    }
+    
+
+
+
+
+
+
+
+
 
 
 
