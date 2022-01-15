1907e630ba52e057a8c143d09e8c47b790438635
admin <admin@MacBook-Pro-admin.local>
2022-01-15 08:43:56 +0200

    fix git settings

---
+++ hw_3/js/main.js
@@ -0,0 +1,25 @@
+// const n = prompt("n");
+// function getMaxDigit(n){
+//     if(n == 0) {
+//         return 0;
+//     }else{
+//         return Math.max(n%10, getMaxDigit(n/10));
+//     }
+// }
+
+// const floor = Math.floor(getMaxDigit(n))
+// console.log(floor);
+
+//<========завдання два ??????======>//
+
+// let str = prompt("srting");
+// function bigLetter(str){
+//     return str[0].toUpperCase()+str.substring(1).toLowerCase();
+// }
+// console.log(bigLetter(str));
+
+const n = 1000;
+function taxValue(n){(n - 19%);
+
+}
+console.log(taxValue(n));
