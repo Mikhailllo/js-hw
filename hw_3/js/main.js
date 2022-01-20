0e07c44d2d1f5af6fb1c1b29fda45be7719cc4d0
Potichniy <smokejoyin.gmail.com>
2022-01-21 00:56:48 +0200

    new tasks hw3

--- hw_3/js/main.js
+++ hw_3/js/main.js
@@ -1,3 +1,7 @@
+
+
+// <========завдання ОДИН ======> YES   
+
 // const n = prompt("n");
 // function getMaxDigit(n){
 //     if(n == 0) {
@@ -10,16 +14,67 @@
 // const floor = Math.floor(getMaxDigit(n))
 // console.log(floor);
 
-//<========завдання два ??????======>//
+//<========завдання ДВА ======>// NOW
 
-// let str = prompt("srting");
-// function bigLetter(str){
-//     return str[0].toUpperCase()+str.substring(1).toLowerCase();
+
+
+//<========завдання ТРИ ======> YES  
+
+// const str = prompt("string");
+// const bigLetter = (string) => {
+//     return string[0].toUpperCase() + string.substring(1).toLowerCase();
 // }
 // console.log(bigLetter(str));
 
-const n = 1000;
-function taxValue(n){(n - 19%);
+//<========завдання ЧОТИРИ ======>// NOW
 
-}
-console.log(taxValue(n));
+// const wages = 6500;
+//     const taxValue/100 - 19%  => {
+  
+
+//     }
+
+//     console.log(taxValue(wages));
+
+//<========завдання П'ять ======>// YES
+
+// const n = 1;
+// const m = 100;
+// function getRandomNumber(n,m) {
+//     let rand = n + Math.random() * (m + 1 - n);
+//     return Math.floor(rand);
+
+// }
+// console.log(getRandomNumber(n,m));
+
+// <========завдання Щість ======>  YES  
+
+// const str = ('Astaloaavista') ;
+//     const countLetter = (str) => {
+//         // 
+//         return repeated = str.replace(/[^a]/g, "" ).length;
+//     }
+
+// console.log(countLetter(str));
+
+// <========завдання СІМ ======>    NOW
+
+const quantity = (100);
+    const convertCurrency = (sum) => {
+        const usdQuan = quantity.addEventListener();
+
+    }
+    console.log(convertCurrency()
+
+
+
+//<========завдання 10 ======>// YES
+
+// const str = prompt("polindrom");
+// const isPalyndrom = (string) => {
+//     const joinedString = string.toLowerCase().split(" ").join("");
+//     return joinedString === joinedString.split('').reverse().join("");
+    
+
+// }
+// console.log(isPalyndrom("Я несу гуся"));
