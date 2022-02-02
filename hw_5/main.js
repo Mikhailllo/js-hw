98bcb3abdc41ed6957794a05a96b0dcd29e01d1a
Potichniy <smokejoyin.gmail.com>
2022-02-02 23:19:50 +0200

    solved the exercise N3

--- hw_5/main.js
+++ hw_5/main.js
@@ -1,5 +1,5 @@
-//  #1
-const randomNumb = () => Math.round(Math.random() * (99 - 5) + 1);
+//  #1 
+const randomNumb = () => Math.round(Math.random() * (50 - 5) + 1);
 const getRandomArray = () => {
 const arrNubers = [];
 for(let = i = 0; i < 15; i++){
@@ -8,3 +8,19 @@ for(let = i = 0; i < 15; i++){
 return arrNubers
 }
 console.log(getRandomArray())
+// #2 noWork
+// const getModa = () => {
+//     const moNumbers = [...getRandomArray()];
+// for(let = i = 0; i > 0; i++){
+//     moNumbers2.push( i + (i -i-1)/((i-i-1)+(i-i+1)))
+// }
+// return moNumbers
+// }
+// console.log(getModa())
+// #3
+const numbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
+const items = numbers.length;
+const middle = numbers.reduce((acc,el,i,arr) => {
+return acc + el ;    
+} )
+console.log(middle / items) ;
