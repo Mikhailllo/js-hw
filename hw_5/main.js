ab885d5d3ff2d9a8bfaa783ff0a24d5ade1f027c
Potichniy <smokejoyin.gmail.com>
2022-02-04 23:50:11 +0200

    add hw5 #7

--- hw_5/main.js
+++ hw_5/main.js
@@ -1,26 +1,53 @@
-//  #1 
+ // #1 
 const randomNumb = () => Math.round(Math.random() * (50 - 5) + 1);
 const getRandomArray = () => {
-const arrNubers = [];
-for(let = i = 0; i < 15; i++){
-    arrNubers.push(randomNumb())
+const arrNumbers = [];
+for(let i = 0; i < 15; i++){
+    arrNumbers.push(randomNumb())
 }
-return arrNubers
+return arrNumbers
 }
 console.log(getRandomArray())
-// #2 noWork
-// const getModa = () => {
-//     const moNumbers = [...getRandomArray()];
-// for(let = i = 0; i > 0; i++){
-//     moNumbers2.push( i + (i -i-1)/((i-i-1)+(i-i+1)))
-// }
-// return moNumbers
-// }
-// console.log(getModa())
-// #3
-const numbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2];
+
+//# 1.1
+// const randomNumb = () => Math.round(Math.random() * (50 - 5) + 1);
+// const arrNumbers = [ 0 ];
+// const getRandomArray = () => arrNumbers.reduce((acc,el).Math.random() * (50 - 5) + 1 );
+// return el = 0 ; el < 15; el++
+// console.log(getRandomArray())
+
+
+//  #3
+const numbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 1.5,2.5];
 const items = numbers.length;
-const middle = numbers.reduce((acc,el,i,arr) => {
-return acc + el ;    
-} )
-console.log(middle / items) ;
+let notIntegerLength =  0;
+const integerArr = numbers.filter(num => Number.isInteger(num) )
+const middle = integerArr.reduce((acc,el) => acc + el);
+console.log(middle / integerArr.length)
+//     //if  (Number.isInteger(el)) {
+//         return acc + el  
+//     //} else {
+//     //    return acc
+//     }
+// } )
+// console.log(middle / (items - notIntegerLength))  ;
+
+//  #5
+const numbersRow = [4,5,7,,9,8,96,55,54,63,40,45,67,890];
+const filterEventNumbers = () => numbersRow.filter((el) => el % 2 === 1); 
+console.log(filterEventNumbers())
+
+//  #6
+const numberArr = [4,-5,7,9,-8,96,54,-63,45,5];
+const positiveNumbers = () => numberArr.filter((el) => el > 0 );
+console.log(positiveNumbers().length)
+
+//  #7
+const dividedByFive = () => numbersRow.filter((el,i,arr) => el % 5 === 0); 
+console.log(dividedByFive())
+
+//  #8
+const str = 'Are you fucking kidding?';
+const arr1 = str.split([" "]);
+const replaceBadWords = () => arr1.join((" "));
+console.log(replaceBadWords().replace('fuck','****') )
