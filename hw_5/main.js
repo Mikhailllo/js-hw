65bb8af5c4a037adfbdfe074abd0cb07b0196f09
Potichniy <smokejoyin.gmail.com>
2022-02-22 15:58:39 +0200

    completed and corrected HW 5 and 6

--- hw_5/main.js
+++ hw_5/main.js
@@ -3,43 +3,52 @@ const randomNumb = () => Math.round(Math.random() * (50 - 5) + 1);
 const getRandomArray = () => {
 const arrNumbers = [];
 for(let i = 0; i < 15; i++){
-    arrNumbers.push(randomNumb())
+    arrNumbers.push(randomNumb());
 }
 return arrNumbers
 }
-console.log(getRandomArray())
+console.log(" #1 : ",getRandomArray());
 
 //  #3
 const numbers = [6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2, 1.5,2.5];
 const items = numbers.length;
 let notIntegerLength =  0;
-const integerArr = numbers.filter(num => Number.isInteger(num) )
+const integerArr = numbers.filter(num => Number.isInteger(num));
 const middle = integerArr.reduce((acc,el) => acc + el);
-console.log(middle / integerArr.length)
+console.log(" #3 : ",middle / integerArr.length);
 
 //  #5
 const numbersRow = [4,5,7,,9,8,96,55,54,63,40,45,67,890];
 const filterEventNumbers = () => numbersRow.filter((el) => el % 2 === 1); 
-console.log(filterEventNumbers())
+console.log(" #5 : ",filterEventNumbers());
 
 //  #6
 const numberArr = [4,-5,7,9,-8,96,54,-63,45,5];
 const positiveNumbers = () => numberArr.filter((el) => el > 0 );
-console.log(positiveNumbers().length)
+console.log(" #6 : ",positiveNumbers().length);
 
 //  #7
-const dividedByFive = () => numbersRow.filter((el,i,arr) => el % 5 === 0); 
-console.log(dividedByFive())
+function dividedByFive() {
+    return numbersRow.filter((el, i, arr) => el % 5 === 0);
+} 
+console.log(" #7 : ",dividedByFive());
 
 //  #8
 const str = 'Are you fucking kidding?';
 const arr1 = str.split([" "]);
 const replaceBadWords = () => arr1.join((" "));
-console.log(replaceBadWords().replace('fuck','****') )
+console.log(" #8 : ",replaceBadWords().replace('fuck','****'));
 
-// №9
-// const word = ['Commander'];
-
-// const divideByThree = () => word.reduce((acc,el,i) => acc[0].length < 3 + el[i],0);
-
-// console.log(divideByThree())
+// #9
+const divideByThree = (word)  => {
+    const string = word.toLowerCase().split(' ').join('');
+    if (string.length < 3) return string;
+    else {
+        const result = [];
+        for (let i = 0; i < string.length; i = i+3) {
+            result.push(string.substr(i,3));
+        }
+        return result;
+    }
+}
+console.log(" #9 : ",divideByThree("Commander"));
--- hw_6/main.js
+++ hw_6/main.js
@@ -4,7 +4,7 @@ const students = [{
     subjects: {
         math: [4, 4, 3, 4],
         algorithms: [3, 3, 3, 4, 4, 4],
-        data_science: [5, 5, 3, 4]
+        data_science: [5, 5, 3, 4],
     }
 }, {
     name: "Victor",
@@ -12,7 +12,7 @@ const students = [{
     subjects: {
         physics: [5, 5, 5, 3],
         economics: [2, 3, 3, 3, 3, 5],
-        geometry: [5, 5, 2, 3, 5]
+        geometry: [5, 5, 2, 3, 5],
     }
 }, {
     name: "Anton",
@@ -20,7 +20,7 @@ const students = [{
     subjects: {
         statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
         english: [5, 3],
-        cosmology: [5, 5, 5, 5]
+        cosmology: [5, 5, 5, 5],
     }
     
 }];
@@ -31,39 +31,53 @@ const getSubjects = (student) => {
     const result = subjectStud.map(i => i.charAt(0).toUpperCase() + i.substr(1).replace(/_/gi," "));
     return result
 }
-console.log(getSubjects(students[0]))
+console.log("#1 ",getSubjects(students[0]));
 
 // #2
-const getAverageMark = (student) => {
-    const allMarksForStudent = Object.values(student.subjects).flat(1)
-    const sum = allMarksForStudent.reduce((total,i) => total +=i)
-    const marksNum = allMarksForStudent.reduce(total => total +=1, 0)
-    return (sum/marksNum).toFixed(2)
+function getAverageMark(student) {
+    const allMarksForStudent = Object.values(student.subjects).flat(1);
+    const sum = allMarksForStudent.reduce((total, i) => total += i);
+    const marksNum = allMarksForStudent.reduce(total => total += 1, 0);
+    return (sum / marksNum).toFixed(2);
 }
-console.log(getAverageMark(students[0]))
+console.log("#2 ",getAverageMark(students[0]));
 
 // #3
-const getStudentsInfo = (student) => {
-	return {
+function getStudentsInfo(student) {
+    return {
         name: student.name,
-		course: student.course,
-		averageMark: getAverageMark(student),
-	};
-};
-console.log(getStudentsInfo(students[0]));
+        course: student.course,
+        averageMark: getAverageMark(student),
+    };
+}
+console.log("#3 ",getStudentsInfo(students[0]));
 
 // #4
 const getStudNames = (students) => {
-    const studentsNames = students.map((student) => student.name)
-    return studentsNames.sort()
+    const studentsNames = students.map((student) => student.name);
+    return studentsNames.sort();
 };
-console.log(getStudNames(students));
+console.log("#4 ",getStudNames(students));
 
 //  #5
 const getBestStudent = (students) => {
 	const averageRating = students.map((student) => Number(getAverageMark(student)));
 	return students[averageRating.indexOf(Math.max(...averageRating))].name;
 };
-console.log(getBestStudent(students));
+console.log("#5 ",getBestStudent(students));
 
 // #6
+const calculateWordLetters = function(word) {
+    const calculated = {};
+    let counter = 1;
+    const arr = word.split("").sort().reverse();
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] == arr[i+1]) counter ++;
+        else {
+            calculated[arr[i]] = counter;
+            counter = 1;
+        }
+    }
+    return calculated;
+}
+console.log("#6 ",calculateWordLetters("test"));
