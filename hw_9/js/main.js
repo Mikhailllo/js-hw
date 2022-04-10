888c3c26ac16dcf2f0598ee8ad68bda423e0e9da
Potichniy <smokejoyin.gmail.com>
2022-04-10 18:26:48 +0300

    hw_9 cubeRubik

--- hw_9/js/main.js
+++ hw_9/js/main.js
@@ -1,12 +1,19 @@
-const cube = document.getElementById("cube-rubik");
+const cubeRubik = document.getElementById("cube-rubik");
+const randomColor = () => {
+	return (
+		"#" +
+		Math.floor(Math.random() * 11111111)
+			.toString(16)
+			.padStart(6, "0")
+			.toUpperCase()
+	);
+};
 
-const randomCalor= () => {
-    return (Math.floor(Math.random() * 222222));
-}
 setInterval(() => {
-for(let i = 0; i < 25; i++){
-    const inner = document.createElement("div");
-    cube.appendChild(inner);
-    inner.style.cssText = "height:50px; width: 50px;background:${randColor()}";
-    }
+	cubeRubik.innerHTML = '';
+	for (let i = 0; i < 25; i++) {
+		const inner = document.createElement("div");
+		cubeRubik.appendChild(inner);
+		inner.style.cssText = `height: 50px; width: 50px; background: ${randomColor()}`;
+	}
 }, 1000);
