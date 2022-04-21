f3f4217a01ae4b7e9f03e413f7b416291b073436
Potichniy <smokejoyin.gmail.com>
2022-04-22 01:47:43 +0300

    hw10

--- hw_10/js/main.js
+++ hw_10/js/main.js
@@ -1,11 +1,23 @@
-const cubeRubik = document.getElementById("cube-rubik");
+const buttonNodeList = document.querySelectorAll(".btn");
+const btncont = document.querySelector(".buttonMixer");
 
+const soundArr = ["bumboks", "kolaba", "rave", "rusya"];
+const letterArray = ["A", "S", "D", "F",];
+const soundArray = soundArr.map((item, index) => {
+    const sound = new Audio(`./sound/sounds_${item}.mp3`);
+    sound.setAttribute("id", `btn-${index + 1}`);
+    sound.setAttribute("code", "Key" + letterArray[index]);
+    return sound;
+});
+document.addEventListener("keydown", (event) => {
+    console.trace();
+    soundArray.forEach(
+        (item) => event.code === item.attributes.code.value && item.play()
+    );
+});
 
-
-	cubeRubik.innerHTML = '';
-	for (let i = 0; i < 10; i++) {
-		const inner = document.createElement("div");
-		cubeRubik.appendChild(inner);
-		inner.style.cssText = `height: 100px; width: 100px; background: #335577`;
-	}
+btncont.addEventListener("click", (event) => {
+    console.trace();
+    soundArray.forEach((item) => event.target.id === item.id && item.play());
+});
 
