e412aa00dc64b1ed0816f9d47fb86492d31da314
Potichniy <smokejoyin.gmail.com>
2022-04-29 15:26:10 +0300

    hw_10

--- hw_10/js/main.js
+++ hw_10/js/main.js
@@ -1,10 +1,10 @@
-const buttonNodeList = document.querySelectorAll(".btn");
+const buttonNodeList = document.querySelectorAll(".button");
 const btncont = document.querySelector(".buttonMixer");
-const soundArr = ["barabannyj", "flejty", "ritmic", "zaciklilo","coolegio","probass","verka","vidbiy","vova","zsu"];
-const letterArray = ["A", "S", "D", "F","G","H","J","k"];
+const soundArr = ["bumboks", "kolaba", "rave", "rusya","coolegio","probass","verka","vidbiy","vova","zsu"];
+const letterArray = ["A", "S", "D", "F","G","H","J","K"];
 const soundArray = soundArr.map((item, index) => {
     const sound = new Audio(`./sound/sounds_${item}.mp3`);
-    sound.setAttribute("id", `btn-${index + 1}`);
+    sound.setAttribute("id", `button-${index + 1}`);
     sound.setAttribute("code", "Key" + letterArray[index]);
     return sound;
 });
